35b95fb74e710f5d8ebce8ea004edcc273aaf852
Neha <nehadilkash@gmail.com>
2022-11-10 21:14:24 +0530

    chapter 18

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
 public class Main {
+
+    public static void main(String[] args) {
+        int[] a = {5, 6, 7, 1, 2, 8, 4};
+        System.out.println(getMax(a, 0));
+    }
+
+    public static int getMax(int[] a, int i) {
+        if (i < a.length-1) {
+            return Math.max(getMax(a, i + 1),a[i]);
+        } else {
+           return  Math.max(a[i],a[i-1]);
+        }
+    }
 }
+
