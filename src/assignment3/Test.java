081eeabd7f657b3bf0d21add2ac7ccfe16eba593
Neha <nehadilkash@gmail.com>
2022-11-24 20:55:05 +0530

    assignment done

--- src/assignment3/Test.java
+++ src/assignment3/Test.java
@@ -25,11 +25,11 @@ public class Test {
     }
 
 
-    public static <GeometricObject> void selectionSort(GeometricObject[] list,
-                                                       Comparator<? super GeometricObject> comparator) {
+    public static <E> void selectionSort(E[] list,
+                                         Comparator<? super E> comparator) {
         for (int i = 0; i < list.length - 1; i++) {
             // Find the minimum in the list[i..list.length-1]
-            GeometricObject currentMin = list[i];
+            E currentMin = list[i];
             int currentMinIndex = i;
 
             for (int j = i + 1; j < list.length; j++) {
